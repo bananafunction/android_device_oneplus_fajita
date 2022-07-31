ad2cc25a836ef296f70166df3f0d870810a489ac
bananafunction <terathree2@web.de>
2022-07-31 18:05:19 +0200

    fajita: Update ApplyPatch.sh
    ... in case any argument is given, do not play around and
    reset defined number of commits hard ...

--- patches/ApplyPatch.sh
+++ patches/ApplyPatch.sh
@@ -5,24 +5,45 @@
 
 PATCH_ROOT="$ANDROID_BUILD_TOP/device/oneplus/fajita/patches"
 
-cd "$ANDROID_BUILD_TOP/system/security"
-git am "$PATCH_ROOT/0001-keystore-Block-key-attestation-for-Google-Play-Servi_SysSec.patch"
-git am "$PATCH_ROOT/0002-keystore-Report-rate-limited-instead-of-not-implemen_SysSec.patch"
+if [[ $# -eq 0 ]]
+    then
+    cd "$ANDROID_BUILD_TOP/system/security"
+    git am "$PATCH_ROOT/0001-keystore-Block-key-attestation-for-Google-Play-Servi_SysSec.patch"
+    git am "$PATCH_ROOT/0002-keystore-Report-rate-limited-instead-of-not-implemen_SysSec.patch"
 
-cd "$ANDROID_BUILD_TOP/frameworks/base"
-git am "$PATCH_ROOT/0001-KeyStore-Block-key-attestation-for-Google-Play-Servi_FraBas.patch"
-git am "$PATCH_ROOT/0002-KeyStore-Report-rate-limited-instead-of-not-implemen_FraBas.patch"
+    cd "$ANDROID_BUILD_TOP/frameworks/base"
+    git am "$PATCH_ROOT/0001-KeyStore-Block-key-attestation-for-Google-Play-Servi_FraBas.patch"
+    git am "$PATCH_ROOT/0002-KeyStore-Report-rate-limited-instead-of-not-implemen_FraBas.patch"
 
-cd "$ANDROID_BUILD_TOP/system/core"
-git am "$PATCH_ROOT/0001-init-Weaken-property-override-security-for-the-init-_SysCore.patch"
-git am "$PATCH_ROOT/0002-init-workaround-SafetyNet-check_SysCore.patch"
-git am "$PATCH_ROOT/0003-init-add-vendor.-keys-to-spoof-safetynet_SysCore.patch"
-git am "$PATCH_ROOT/0004-init-Weaken-property-override-security-only-when-spo_SysCore.patch"
-git am "$PATCH_ROOT/0005-init-Only-set-safetynet-props-if-not-eng-build_SysCore.patch"
-git am "$PATCH_ROOT/0006-core-Add-more-props-for-snet-spoofing_SysCore.patch"
-git am "$PATCH_ROOT/0007-core-Don-t-spoof-props-in-recovery-mode_SysCore.patch"
+    cd "$ANDROID_BUILD_TOP/system/core"
+    git am "$PATCH_ROOT/0001-init-Weaken-property-override-security-for-the-init-_SysCore.patch"
+    git am "$PATCH_ROOT/0002-init-workaround-SafetyNet-check_SysCore.patch"
+    git am "$PATCH_ROOT/0003-init-add-vendor.-keys-to-spoof-safetynet_SysCore.patch"
+    git am "$PATCH_ROOT/0004-init-Weaken-property-override-security-only-when-spo_SysCore.patch"
+    git am "$PATCH_ROOT/0005-init-Only-set-safetynet-props-if-not-eng-build_SysCore.patch"
+    git am "$PATCH_ROOT/0006-core-Add-more-props-for-snet-spoofing_SysCore.patch"
+    git am "$PATCH_ROOT/0007-core-Don-t-spoof-props-in-recovery-mode_SysCore.patch"
 
-cd "$ANDROID_BUILD_TOP/device/oneplus/fajita"
-git am "$PATCH_ROOT/0001-fajita-use-fingerprint-of-Pixel-device.patch"
+    cd "$ANDROID_BUILD_TOP/device/oneplus/fajita"
+    git am "$PATCH_ROOT/0001-fajita-use-fingerprint-of-Pixel-device.patch"
 
-echo "Done."
+    echo "Done."
+    exit 1
+else
+
+    cd "$ANDROID_BUILD_TOP/system/security"
+    git reset --hard HEAD~2
+    
+    cd "$ANDROID_BUILD_TOP/frameworks/base"
+    git reset --hard HEAD~3
+    
+    cd "$ANDROID_BUILD_TOP/system/core"
+    git reset --hard HEAD~7
+    
+    cd "$ANDROID_BUILD_TOP/packages/modules/DnsResolver"
+    git reset --hard HEAD~1
+    
+    cd "$ANDROID_BUILD_TOP/device/oneplus/fajita"
+    git reset --hard HEAD~1
+    exit 1
+fi
