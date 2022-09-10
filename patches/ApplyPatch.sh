977145ea20e89a08db6a4aeb3017ccbd71b71a30
bananafunction <terathree2@web.de>
2022-09-10 10:56:12 +0200

    Patches: Update for crDroid-8

--- patches/ApplyPatch.sh
+++ patches/ApplyPatch.sh
@@ -7,39 +7,15 @@ PATCH_ROOT="$ANDROID_BUILD_TOP/device/oneplus/fajita/patches"
 
 if [[ $# -eq 0 ]]
     then
-    cd "$ANDROID_BUILD_TOP/frameworks/base"
-    git am "$PATCH_ROOT/0001-Spoof-build-fingerprint-for-Google-Play-Services_FraBase.patch"
-    git am "$PATCH_ROOT/0002-keystore-Block-key-attestation-for-SafetyNet_FraBase.patch"
-    git am "$PATCH_ROOT/0003-Limit-SafetyNet-workarounds-to-unstable-GMS-process_FraBase.patch"
-
-    cd "$ANDROID_BUILD_TOP/system/core"
-    git am "$PATCH_ROOT/0001-tighten-up-mount-permissions_SysCore.patch"
-    git am "$PATCH_ROOT/0002-enable-kernel-mitigations-for-link-races_SysCore.patch"
-    git am "$PATCH_ROOT/0003-enable-kernel-mitigations-for-file-spoofing_SysCore.patch"
-    git am "$PATCH_ROOT/0004-libfs_avb-Disable-dm-verity-when-AVB-is-permissive_SysCore.patch"
-    git am "$PATCH_ROOT/0005-init-Report-valid-verified-boot-for-SafetyNet-checks_SysCore.patch"
-
     cd "$ANDROID_BUILD_TOP/packages/apps/Updater"
     git am "$PATCH_ROOT/0001-Define-new-server-URL_Updater.patch"
     git am "$PATCH_ROOT/0002-Define-new-changelog-URL_Updater.patch"
 
-    cd "$ANDROID_BUILD_TOP/device/oneplus/fajita"
-    git am "$PATCH_ROOT/0001-fajita-update-fingerprint-patch-to-match-SPL.patch"
-
     echo "Done."
     exit 1
 else
-    cd "$ANDROID_BUILD_TOP/frameworks/base"
-    git reset --hard HEAD~3
-
-    cd "$ANDROID_BUILD_TOP/system/core"
-    git reset --hard HEAD~5
-
     cd "$ANDROID_BUILD_TOP/packages/apps/Updater"
     git reset --hard HEAD~2
-
-    cd "$ANDROID_BUILD_TOP/device/oneplus/fajita"
-    git reset --hard HEAD~1
 
     exit 1
 fi
